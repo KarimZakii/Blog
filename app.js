1b4e4101b05c622a56dfe78d5fe74245c6c2e557
KarimZakii <karimzaki686@gmail.com>
2023-11-25 20:31:11 +0200

    Adding gitignore and app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
